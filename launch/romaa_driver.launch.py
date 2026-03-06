77a52666680ca8c4ed463b3a2e86b4aae6a7c2d2
Gonzalo Perez-Paina <gfperezpaina@gmail.com>
2026-03-06 10:28:23 -0300

    Add YAML configuration file for node params
    The launch file is also modified to load the configuration
    file with the node parameters.

--- launch/romaa_driver.launch.py
+++ launch/romaa_driver.launch.py
@@ -1,61 +1,22 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
 
-    frequency_arg = DeclareLaunchArgument(
-        'frequency', default_value='10.0',
-        description='Publisher frequency')
-
-    port_arg = DeclareLaunchArgument(
-        'port', default_value='/dev/ttyUSB0',
-        description='Serial port device')
-
-    baudrate_arg = DeclareLaunchArgument(
-        'baudrate', default_value='115200',
-        description='Serial port baud-rate')
-
-    odom_frame_arg = DeclareLaunchArgument(
-        'odom_frame', default_value='odom',
-        description='Odometry frame')
-
-    base_frame_arg = DeclareLaunchArgument(
-        'base_frame', default_value='base_link',
-        description='Robot frame')
-
-    enable_motor_arg = DeclareLaunchArgument(
-        'enable_motor', default_value='False',
-        description='Enable/disable motors at node startup')
-
-    reset_odom_arg = DeclareLaunchArgument(
-        'reset_odom', default_value='False',
-        description='Reset odometry at node startup')
-
-    romaa_driver_node = Node(
-        package = 'romaa_driver',
-        name = 'romaa_driver',
-        executable = 'romaa_driver',
-        parameters = [
-            {"frequency": LaunchConfiguration('frequency')},
-            {"port": LaunchConfiguration('port')},
-            {"baudrate": LaunchConfiguration('baudrate')},
-            {"odom_frame": LaunchConfiguration('odom_frame')},
-            {"base_frame": LaunchConfiguration('base_frame')},
-            {"enable_motor": LaunchConfiguration('enable_motor')},
-            {"reset_odom": LaunchConfiguration('reset_odom')}
-        ],
-        output="screen"
-    )
-
+    config_path = PathJoinSubstitution([
+        FindPackageShare('romaa_driver'),
+        'config',
+        'params.yaml'
+    ])
+    
     return LaunchDescription([
-        frequency_arg,
-        port_arg,
-        baudrate_arg,
-        odom_frame_arg,
-        base_frame_arg,
-        enable_motor_arg,
-        reset_odom_arg,
-        romaa_driver_node
+        Node(
+            package = 'romaa_driver',
+            name = 'romaa_driver',
+            executable = 'romaa_driver',
+            parameters = [config_path]
+        )
+
     ])
